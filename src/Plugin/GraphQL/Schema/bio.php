c6c3e99757a3ef06196968b6892be76ad8c155b7
Arman <amikaili@uiowa.edu>
2020-10-28 10:15:49 -0500

    Initial release php

---
+++ src/Plugin/GraphQL/Schema/bio.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\my_graphql\Plugin\GraphQL\Schema;
+
+use Drupal\graphql\GraphQL\ResolverBuilder;
+use Drupal\graphql\GraphQL\ResolverRegistry;
+use Drupal\graphql\Plugin\GraphQL\Schema\SdlSchemaPluginBase;
+use Drupal\my_graphql\Wrappers\QueryConnection;
+
+/**
+ * @Schema(
+ *   id = "b",
+ *   name = "Bios Schema"
+ * )ß
+ */
+class BiosSchema extends SdlSchemaPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getResolverRegistry() {
+    $builder = new ResolverBuilder();
+    $registry = new ResolverRegistry();
+
+    $this->addQueryFields($registry, $builder);
+    $this->addBioFields($registry, $builder);
+
+    // Re-usable connection type fields.
+    $this->addConnectionFields('BioConnection', $registry, $builder);
+
+    return $registry;
+  }
+
+  /**
+   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
+   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
+   */
+  protected function addBioFields(ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver('Bio', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('Bio', 'field_role_detailed',
+      $builder->compose(
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent()),
+        $builder->produce('uppercase')
+          ->map('string', $builder->fromParent())
+      )
+    );
+
+    $registry->addFieldResolver('Bio', 'field_workstream_role',
+      $builder->compose(
+        $builder->produce('entity_owner')
+          ->map('entity', $builder->fromParent()),
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent())
+      )
+    );
+  }
+
+  /**
+   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
+   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
+   */
+  protected function addQueryFields(ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver('Query', 'bio',
+      $builder->produce('entity_load')
+        ->map('type', $builder->fromValue('node'))
+        ->map('bundles', $builder->fromValue(['bio']))
+        ->map('id', $builder->fromArgument('id'))
+    );
+
+    $registry->addFieldResolver('Query', 'bios',
+      $builder->produce('query_bios')
+        ->map('offset', $builder->fromArgument('offset'))
+        ->map('limit', $builder->fromArgument('limit'))
+    );
+  }
+
+  /**
+   * @param string $type
+   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
+   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
+   */
+  protected function addConnectionFields($type, ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver($type, 'total',
+      $builder->callback(function (QueryConnection $connection) {
+        return $connection->total();
+      })
+    );
+
+    $registry->addFieldResolver($type, 'items',
+      $builder->callback(function (QueryConnection $connection) {
+        return $connection->items();
+      })
+    );
+  }
+}
