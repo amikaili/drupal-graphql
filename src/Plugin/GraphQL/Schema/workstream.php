e441e179046d1bcb35753f421af6e267c839ef71
Arman <amikaili@uiowa.edu>
2020-10-28 10:09:39 -0500

    Initial release php

---
+++ src/Plugin/GraphQL/Schema/workstream.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\my_graphql\Plugin\GraphQL\Schema;
+
+use Drupal\graphql\GraphQL\ResolverBuilder;
+use Drupal\graphql\GraphQL\ResolverRegistry;
+use Drupal\graphql\Plugin\GraphQL\Schema\SdlSchemaPluginBase;
+use Drupal\my_graphql\Wrappers\QueryConnection;
+
+/**
+ * @Schema(
+ *   id = "ws",
+ *   name = "Workstream Schema"
+ * )ß
+ */
+class WorkstreamSchema extends SdlSchemaPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getResolverRegistry() {
+    $builder = new ResolverBuilder();
+    $registry = new ResolverRegistry();
+
+    $this->addQueryFields($registry, $builder);
+    $this->addWorkstreamFields($registry, $builder);
+
+    // Re-usable connection type fields.
+    $this->addConnectionFields('WorkstreamConnection', $registry, $builder);
+
+    return $registry;
+  }
+
+  /**
+   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
+   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
+   */
+  protected function addWorkstreamFields(ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver('Workstream', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('Workstream', 'field_mission',
+      $builder->compose(
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent()),
+        $builder->produce('uppercase')
+          ->map('string', $builder->fromParent())
+      )
+    );
+
+    $registry->addFieldResolver('Workstream', 'body',
+      $builder->compose(
+        $builder->produce('entity_owner')
+          ->map('entity', $builder->fromParent()),
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent())
+      )
+    );
+  }
+
+  /**
+   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
+   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
+   */
+  protected function addQueryFields(ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver('Query', 'workstream',
+      $builder->produce('entity_load')
+        ->map('type', $builder->fromValue('node'))
+        ->map('bundles', $builder->fromValue(['workstream']))
+        ->map('id', $builder->fromArgument('id'))
+    );
+
+    $registry->addFieldResolver('Query', 'workstreams',
+      $builder->produce('query_workstreams')
+        ->map('offset', $builder->fromArgument('offset'))
+        ->map('limit', $builder->fromArgument('limit'))
+    );
+  }
+
+  /**
+   * @param string $type
+   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
+   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
+   */
+  protected function addConnectionFields($type, ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver($type, 'total',
+      $builder->callback(function (QueryConnection $connection) {
+        return $connection->total();
+      })
+    );
+
+    $registry->addFieldResolver($type, 'items',
+      $builder->callback(function (QueryConnection $connection) {
+        return $connection->items();
+      })
+    );
+  }
+}
